58436255adebb9536bc9bd49fbfbe24af44ffc27
Adil Shamuratov <iswearican_1@outlook.com>
2023-04-06 18:34:40 +0600

    clear

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ services = BaseServices()
 @app.post("/")
 async def root():
     for file in os.listdir('file_db/'):
-        print(file)
         if 'Штурм' in file:
             list_name = 'ПРАЙС'
         if 'PIT' in file:
+            print(file)
             list_name = 'Инструменты'
         file_name = str(int(time.time())) + '.csv'
 
